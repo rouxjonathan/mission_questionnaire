142b8ec0c0a4f2b071c6e8ee40af1572cd5b3b4f
Jonathan <rouxjonathan@icloud.com>
2021-09-11 16:20:24 +0700

    améliorations : commentaires et gestion d'erreurs

--- import.py
+++ import.py
@@ -24,7 +24,7 @@ def generate_json_file(categorie, titre, url):
     try:
         response = requests.get(url)
     except:
-        print("Exception pour la requete : " + url)
+        print("Exception pour la requete HTTP GET : " + url)
     else:
         try:
             data = json.loads(response.text)
@@ -48,7 +48,7 @@ def generate_json_file(categorie, titre, url):
                 file.close()
                 print("end")
         except:
-            print("Exception data pour l'url : " + url + " - Questionnaire : " + titre)
+            print(f"Exception dans la désérialisation ou l'utilisation des données (questionnaire : {titre}, url: {url})")
 
     
 
--- questionnaire.py
+++ questionnaire.py
@@ -8,8 +8,11 @@ class Question:
         self.bonne_reponse = bonne_reponse
 
     def from_json_data(data):
+        # Transforme les données choix tuple (titre, bool 'bonne réponse') -> [choix1, choix2....]
         choix = [i[0] for i in data["choix"]]
+        # Trouve le bon choix en fonction du bool 'bonne réponse'
         bonne_reponse = [i[0] for i in data["choix"] if i[1]]
+        # Si aucune bonne réponse ou plusieurs bonnes réponses -> Anomalie dans les donnees
         if len(bonne_reponse) != 1:
             return None
         q = Question(data["titre"], choix, bonne_reponse[0])
@@ -55,6 +58,8 @@ class Questionnaire:
     def from_json_data(data):
         questionnaire_data_questions =  data["questions"]
         questions = [Question.from_json_data(i) for i in questionnaire_data_questions]
+        # Supprime les questions None (qui n'ont pas pu être crées)
+        questions = [i for i in questions if i]
 
         return Questionnaire(questions, data["categorie"], data["titre"], data["difficulte"])
 
